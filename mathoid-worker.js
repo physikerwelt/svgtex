d02464e6c7726509c23dbf82e1f3664be9b4617f
Gabriel Wicke <wicke@wikidev.net>
2014-10-10 12:54:14 -0700

    Use texvcjs for TeX sanitization
    - return 'sanetex' attribute with the sanitized tex source
    - detabify
    - if the input can not be sanitized an error is returned
    - note: mathoid uses the original user input for the actual
      processing and not sanetex
    
    Change-Id: I20a4431a1277bb2c7a7a8bfadc5a7ef803f7e2ab

--- mathoid-worker.js
+++ mathoid-worker.js
@@ -5,12 +5,13 @@
  */
 // global includes
 var express = require('express'),
-	cluster = require('cluster'),
-	http = require('http'),
-	fs = require('fs'),
-	child_process = require('child_process'),
-	request = require('request'),
-	querystring = require('querystring');
+    cluster = require('cluster'),
+    http = require('http'),
+    fs = require('fs'),
+    child_process = require('child_process'),
+    request = require('request'),
+    querystring = require('querystring');
+var texvcjs = require('texvcjs');
 var mjAPI = require("./MathJaxNode/lib/mj-single.js");
 
 var format = "TeX";
@@ -20,12 +21,12 @@ var config;
 
 // Get the config
 try {
-	config = JSON.parse(fs.readFileSync('./mathoid.config.json', 'utf8'));
+    config = JSON.parse(fs.readFileSync('./mathoid.config.json', 'utf8'));
 } catch ( e ) {
-	// Build a skeleton localSettings to prevent errors later.
-	console.error("Please set up your mathoid.config.json from the example " +
-			"storoid.config.json.example");
-	process.exit(1);
+    // Build a skeleton localSettings to prevent errors later.
+    console.error("Please set up your mathoid.config.json from the example " +
+            "storoid.config.json.example");
+    process.exit(1);
 }
 
 mjAPI.config({MathJax: {SVG: {font: font}, menuSettings: {semantics: true}},
@@ -51,76 +52,96 @@ app.use(express.bodyParser({maxFieldsSize: 25 * 1024 * 1024}));
 app.use( express.limit( '25mb' ) );
 
 app.get('/', function(req, res){
-	res.write('<html><body>\n');
-	res.write('Welcome to Mathoid. POST to / with var <code>tex</code>');
-	res.write('<form action="/" method="POST"><input type="text" name="q"></form>');
-	res.end('</body></html>');
+    res.write('<html><body>\n');
+    res.write('Welcome to Mathoid. POST to / with var <code>tex</code>');
+    res.write('<form action="/" method="POST"><input type="text" name="q"></form>');
+    res.end('</body></html>');
 });
 
 // robots.txt: no indexing.
 app.get(/^\/robots.txt$/, function ( req, res ) {
-	res.end( "User-agent: *\nDisallow: /\n" );
+    res.end( "User-agent: *\nDisallow: /\n" );
 });
 
 
 function handleRequest(req, res, q, type) {
-	var mml = true;
-	//Keep format variables constant
-	if (type === "tex") {
-		type = "TeX";
-	}
-	if (type === "mml" || type === "MathML") {
-		type = "MathML";
-		mml = false;
-	}
-	if (type === "ascii" || type === "asciimath") {
-		type = "AsciiMath";
-	}
-	mjAPI.typeset({math: q, format: type, svg: true, img: true, mml: mml}, function (data) {
-		if (data.errors) {
-			data.success = false;
-			data.log = "Error:" + JSON.stringify(data.errors);
-		} else {
-			data.success = true;
-			data.log = "success";
-		}
+    var mml = true;
+    var sanitizedTex;
+    //Keep format variables constant
+    if (type === "tex") {
+        type = "TeX";
+        var sanitizationOutput = texvcjs.check(q);
+        // XXX properly handle errors here!
+        if (sanitizationOutput.status === '+') {
+            sanitizedTex = sanitizationOutput.output || '';
+            q = sanitizedTex;
+        } else {
+            res.writeHead(400, {
+                'Content-Type': 'application/json'
+            });
+            return res.end(JSON.stringify({
+                success: false,
+                log: sanitizationOutput.status + ': ' + sanitizationOutput.details
+            }));
+        }
 
-		// Temporary work-around for a duplicate attribute (invalid XML)
-		// returned by MathJax.
-		if (data.mml) {
-			data.mml = data.mml.replace(/<mstyle displaystyle="[^"]+" (?=displaystyle)/g, '<mstyle ');
-		}
+    }
+    if (type === "mml" || type === "MathML") {
+        type = "MathML";
+        mml = false;
+    }
+    if (type === "ascii" || type === "asciimath") {
+        type = "AsciiMath";
+    }
+    mjAPI.typeset({math: q, format: type, svg: true, img: true, mml: mml}, function (data) {
+        if (data.errors) {
+            data.success = false;
+            data.log = "Error:" + JSON.stringify(data.errors);
+        } else {
+            data.success = true;
+            data.log = "success";
+        }
 
-		// Strip some styling returned by MathJax
-		if (data.svg) {
-			data.svg = data.svg.replace(/style="([^"]+)"/, function(match, style) {
-					return 'style="'
-						+ style.replace(/(?:margin(?:-[a-z]+)?|position):[^;]+; */g, '')
-						+ '"';
-			});
-		}
+        // Temporary work-around for a duplicate attribute (invalid XML)
+        // returned by MathJax.
+        if (data.mml) {
+            data.mml = data.mml.replace(/<mstyle displaystyle="[^"]+" (?=displaystyle)/g, '<mstyle ');
+        }
 
-		res.writeHead(200,
-			{
-				'Content-Type': 'application/json'
-			});
-		res.end(JSON.stringify(data));
-	});
+        // Strip some styling returned by MathJax
+        if (data.svg) {
+            data.svg = data.svg.replace(/style="([^"]+)"/, function(match, style) {
+                    return 'style="'
+                        + style.replace(/(?:margin(?:-[a-z]+)?|position):[^;]+; */g, '')
+                        + '"';
+            });
+        }
+
+        // Return the sanitized TeX to the client
+        if (sanitizedTex !== undefined) {
+            data.sanetex = sanitizedTex;
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(data));
+    });
 }
 
 
 app.post(/^\/$/, function ( req, res ) {
-	// First some rudimentary input validation
-	if (!(req.body.q)) {
-		res.writeHead(400);
-		return res.end(JSON.stringify({error: "q (query) post parameter is missing!"}));
-	}
-	var q = req.body.q;
+    // First some rudimentary input validation
+    if (!(req.body.q)) {
+        res.writeHead(400);
+        return res.end(JSON.stringify({error: "q (query) post parameter is missing!"}));
+    }
+    var q = req.body.q;
     var type = req.body.type;
     if (!(req.body.type) ){
         type = 'tex';
     }
-	handleRequest(req, res, q, type);
+    handleRequest(req, res, q, type);
 
 });
 
